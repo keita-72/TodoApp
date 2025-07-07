aeafa8509466444e6ac0d18eace412c5969502c5
keita-72 <github@n.needles72@icloud.com>
2025-07-07 21:36:50 +0900

    フロントバックエンド接続成功

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,17 @@ from flask_cors import CORS
 app = Flask(__name__)# Flaskのアプリケーションを作成する
 CORS(app)
 
-@app.route("/api/todos")# ルーティングの設定
-def get_todos():
-    return jsonify([
-        {"id": 1, "title": "勉強する", "done": False},
-        {"id": 2, "title": "買い物に行く", "done": True}
-    ])
+# 仮のTodoリスト
+todos = [
+    {"id":1,"title":"買い物"},
+    {"id":2,"title":"宿題"}
+]
+
+@app.route("/todos/<int:todo_id>",methods=["DELETE"])# ルーティングの設定
+def delete_todo(todo_id):
+    global todos
+    todos = [todo for todo in todos if todo["id"] != todo_id]
+    return jsonify({"message":"削除しました"}),200
 
 if __name__ == "__main__":
     app.run(debug=True)
